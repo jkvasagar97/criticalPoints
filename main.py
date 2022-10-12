f261b3cccfaf024b4604d25a4faf431abd87cc2f
Jeeva Kumar <jeevakba2024@email.iimcal.ac.in>
2022-10-12 16:37:51 +0530

    encapsulated crit points into a class

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import utils.polynomial as pl
-import utils.critpoints as crit
+from utils.critpoints import CritPoints as crit
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-    poly_curve = pl.Polynomial([1,0,0,-1000])
+    poly_curve = pl.Polynomial([1,0,1,-1000])
     x,y = poly_curve.sample(-20,20,60)
     x_crit,y_crit = crit.get_crit_points(x,y,5,0.1)
     plt.plot(x,y)
--- utils/critpoints.py
+++ utils/critpoints.py
@@ -1,40 +1,44 @@
 import numpy as np
 import math
 
-def get_distance(x,y): #probably won't need this
-    '''returns the distance between two points'''
-    return math.dist(x,y)
+class CritPoints:
+    @staticmethod
+    def get_distance(x,y): #probably won't need this
+        '''returns the distance between two points'''
+        return math.dist(x,y)
 
-def get_distance(p1,p2,p3):
-    '''returns the distance between a line drawn 
-    by 1st 2 points and the 3rd point'''
-    p1 = np.asarray(p1)
-    p2 = np.asarray(p2)
-    p3 = np.asarray(p3)
-    return np.linalg.norm(np.cross(p2-p1, p1-p3))/np.linalg.norm(p2-p1)
+    @staticmethod
+    def get_distance(p1,p2,p3):
+        '''returns the distance between a line drawn 
+        by 1st 2 points and the 3rd point'''
+        p1 = np.asarray(p1)
+        p2 = np.asarray(p2)
+        p3 = np.asarray(p3)
+        return np.linalg.norm(np.cross(p2-p1, p1-p3))/np.linalg.norm(p2-p1)
     
-def get_crit_points(x,y, step, cutoff):
-    # For every point we need to 
-        #find the number in the next progression
-        #find the distance of every point to the line connection it 
-        #select that point and back to one
-        
-    crit_x = [x[0]]
-    crit_y = [y[0]]
-    index = 0
-    while(index <= len(x)):
-        next_index = index+step
-        if next_index > len(x) - 1:
-            break
-        for i in range(index, next_index+1):
-            distance = get_distance( [x[index],y[index]], [x[next_index],y[next_index]], [x[i],y[i]])
-            if cutoff < distance :
-                crit_x.append((x[i]))
-                crit_y.append(y[i])
-                index = i
+    @staticmethod
+    def get_crit_points(x,y, step, cutoff):
+        # For every point we need to 
+            #find the number in the next progression
+            #find the distance of every point to the line connection it 
+            #select that point and back to one
+            
+        crit_x = [x[0]]
+        crit_y = [y[0]]
+        index = 0
+        while(index <= len(x)):
+            next_index = index+step
+            if next_index > len(x) - 1:
                 break
-        index = next_index
-    crit_x.append(x[-1])
-    crit_y.append(y[-1])
-    return crit_x, crit_y
+            for i in range(index, next_index+1):
+                distance = CritPoints.get_distance( [x[index],y[index]], [x[next_index],y[next_index]], [x[i],y[i]])
+                if cutoff < distance :
+                    crit_x.append((x[i]))
+                    crit_y.append(y[i])
+                    index = i
+                    break
+            index = next_index
+        crit_x.append(x[-1])
+        crit_y.append(y[-1])
+        return crit_x, crit_y
     
